c23785eeee820fc9d4f72bdcd5b8b7a4831424d6
bigsea116-netizen <bigsea116@gmail.com>
2025-12-31 08:36:31 +0900

    理解のコメント追加

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+//node_modulesにある機能を呼び出して関数、変数化
+//これらのrequireは何が起こっている？→Express アプリを生成するファクトリ関数
 const express = require("express");
 const line = require("@line/bot-sdk");
 
+//環境変数からconfigを作成　環境変数をべた書きするのはよくない
 const config = {
   channelAccessToken: process.env.ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
+//express()を実行して Expressアプリケーション（サーバーインスタンス）を生成
 const app = express();
 
+// /webhook に POST が来たら、LINE の署名検証ミドルウェアを通し、 
+// 受け取ったイベントを handleEvent で処理して返信する
 app.post("/webhook", line.middleware(config), (req, res) => {
   console.log(JSON.stringify(req,res,null,2));
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
@@ -15,18 +21,21 @@ app.post("/webhook", line.middleware(config), (req, res) => {
   );
 });
 
+//テキストメッセージにのみ返信する
 const handleEvent = (event) => {
-    //テキストメッセージにのみ返信する
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
-
   return client.replyMessage(event.replyToken, {
     type: "text",
     text: event.message.text,
   });
 };
 
+//サーバー起動時に1回だけ生成
+//これは必要？→client.replyMessageで使うため必要
 const client=new line.Client(config);
 
+//3000番ポートで待機
+//これを書かないとどうなる？
 app.listen(3000);
