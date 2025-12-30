f2b882a1ec2bb142728f421de10a33d1e4889fa8
bigsea116-netizen <bigsea116@gmail.com>
2025-12-31 08:59:36 +0900

    supabaseを追加

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
 //node_modulesにある機能を呼び出して関数、変数化
 //これらのrequireは何が起こっている？→Express アプリを生成するファクトリ関数
 const express = require("express");
 const line = require("@line/bot-sdk");
+
+//urlとkeyからsupabaseのインスタンスを生成
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
+//IDと内容を引数にもらい、supabaseに保存する関数
+//fromは何をしている？→どのテーブルかを指定している
+async function saveTraningLog(userId, content) {
+  const { data, error } = await supabase.from('traning_log').insert([{user_id: userId, content: content}])
+  if (error) throw error
+  return data;
+}
 
 //環境変数からconfigを作成　環境変数をべた書きするのはよくない
 const config = {
@@ -15,7 +30,7 @@ const app = express();
 // /webhook に POST が来たら、LINE の署名検証ミドルウェアを通し、 
 // 受け取ったイベントを handleEvent で処理して返信する
 app.post("/webhook", line.middleware(config), (req, res) => {
-  console.log(JSON.stringify(req,res,null,2));
+  console.log(JSON.stringify(req, res, null, 2));
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
   );
@@ -34,7 +49,7 @@ const handleEvent = (event) => {
 
 //サーバー起動時に1回だけ生成
 //これは必要？→client.replyMessageで使うため必要
-const client=new line.Client(config);
+const client = new line.Client(config);
 
 //3000番ポートで待機
 //これを書かないとどうなる？
