ef9a09b8b5a75c754aee592aee976384ac64fbec
bigsea116-netizen <bigsea116@gmail.com>
2025-12-31 01:00:40 +0900

    タイプ修正

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const line = require("@line/bot-sdk");
 
 const config = {
   channelAccessToken: process.env.ACCES_TOKEN,
-  channelScret: process.env.CHANNEL_SECRET,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 const app = express();
 
-app.post("/webhook", kine.middleware(config), (req, res) => {
+app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
   );
