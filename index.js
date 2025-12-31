435c5bf2fad45d6a2acb6c3d1c209227417e20f2
bigsea116-netizen <bigsea116@gmail.com>
2025-12-31 12:09:27 +0900

    記録内容を保存できるように修正

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 //これらのrequireは何が起こっている？→Express アプリを生成するファクトリ関数
 const express = require("express");
 const line = require("@line/bot-sdk");
-const {createClient}=require('@supabase/supabase-js')
+const { createClient } = require("@supabase/supabase-js");
+const userState = {};
 
 //urlとkeyからsupabaseのインスタンスを生成
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
-)
+);
 
 //IDと内容を引数にもらい、supabaseに保存する関数
 //fromは何をしている？→どのテーブルかを指定している
 async function saveTraningLog(userId, content) {
-  const { data, error } = await supabase.from('training_log').insert([{user_id: userId, content: content}])
-  if (error) throw error
+  const { data, error } = await supabase
+    .from("training_log")
+    .insert([{ user_id: userId, content: content }]);
+  if (error) throw error;
   return data;
 }
 
@@ -27,24 +30,45 @@ const config = {
 //express()を実行して Expressアプリケーション（サーバーインスタンス）を生成
 const app = express();
 
-// /webhook に POST が来たら、LINE の署名検証ミドルウェアを通し、 
+// /webhook に POST が来たら、LINE の署名検証ミドルウェアを通し、
 // 受け取ったイベントを handleEvent で処理して返信する
 app.post("/webhook", line.middleware(config), (req, res) => {
-  console.log("EVENT:",JSON.stringify(req.body, null, 2));
+  console.log("EVENT:", JSON.stringify(req.body, null, 2));
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
   );
 });
 
 //テキストメッセージにのみ返信する
-const handleEvent = (event) => {
+const handleEvent = async (event) => {
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
-  if(event.message.text==='記録'){
-    return client.replyMessage(event.replyToken,{
-      type: "text", text: "今日のトレーニング内容を教えてください！"
-    })
+  const userId = event.source.userId;
+  const text = event.message.text;
+
+  if (text === "記録") {
+    userState[userId] = "recording";
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: "今日のトレーニング内容を教えてください！",
+    });
+  }
+  if (userState[userId] === "recording") {
+    try {
+      await saveTraningLog(userId, text);
+      delete userState[userId]; 
+      return client.replyMessage(event.replyToken, {
+        type: "text",
+        text: "保存完了しました！",
+      });
+    } catch (error) {
+      console.error(error);
+      return client.replyMessage(event.replyToken, {
+        type: "text",
+        text: "保存に失敗しました…もう一度お試しください",
+      });
+    }
   }
   return client.replyMessage(event.replyToken, {
     type: "text",
@@ -57,5 +81,4 @@ const handleEvent = (event) => {
 const client = new line.Client(config);
 
 //3000番ポートで待機
-//これを書かないとどうなる？
 app.listen(3000);
